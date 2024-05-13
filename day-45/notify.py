2a4f7e092782808f2874ab3e6e4a4dc84c3be831
Misbah yousaf <107263622+Misbahkk@users.noreply.github.com>
2024-05-13 21:59:50 +0500

    Add files via upload

---
+++ day-45/notify.py
@@ -0,0 +1,14 @@
+import time 
+from plyer import notification
+
+if __name__ == '__main__':
+    
+    while True:
+      notification.notify(
+          title= 'Notification',
+          message = 'this is my message for notification',
+          app_name = 'news notify',
+          app_icon = "News_icon.png",
+          timeout =10
+          )
+      time.sleep(15)
---
+++ day-45/string_ex.py
@@ -0,0 +1,12 @@
+# message = "A kong string with a silly typo"
+# message[2] = "l"
+#This will throw an error
+
+#replace the value in string
+message = "Hi i not misbah Yousaf"
+new_message = message[:4]+'am'+message[8:]
+print(new_message)
+split_message = 'This is a banna'.split()
+x='.....'.join(['this','is','not','mango'])
+print(x)
+print(split_message)
