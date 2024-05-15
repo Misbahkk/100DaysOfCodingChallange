7a18b6701e0c6f69cf5a1bbb6a9661b60a26e3e3
Misbah yousaf <107263622+Misbahkk@users.noreply.github.com>
2024-05-15 23:31:36 +0500

    Add files via upload

---
+++ day-46/example.py
@@ -0,0 +1,16 @@
+# x = float(input("enter num: "))
+# y = float(input("enter num: "))
+
+# print(y**(1/x))
+
+
+# x = int(input())
+# y = int(input())
+# x = x % y
+# x = x % y
+# y = y % x
+# print(y)
+
+
+x = 1//5+1/5
+print(x)
