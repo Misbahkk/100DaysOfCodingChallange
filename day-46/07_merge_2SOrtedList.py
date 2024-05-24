5d5de471b6f7d03eeb07f1346e5b4dc14ae0b45a
Misbah yousaf <107263622+Misbahkk@users.noreply.github.com>
2024-05-24 23:01:55 +0500

    Update 07_merge_2SOrtedList.py

--- day-46/07_merge_2SOrtedList.py
+++ day-46/07_merge_2SOrtedList.py
@@ -8,9 +8,12 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-class Solution(object):
+class Solution(object):                      
     def mergeTwoLists(self, list1, list2):
-        """
+        """      
+
+
+                                          
         :type list1: Optional[ListNode]
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
