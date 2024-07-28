bbc71d01b4f020e16304686d1e048aff8c55473f
Misbah yousaf <107263622+Misbahkk@users.noreply.github.com>
2024-07-28 19:17:32 +0500

    Update complex_num_add.py

--- day-58/complex_num_add.py
+++ day-58/complex_num_add.py
@@ -2,7 +2,7 @@ class Complex:
     def __init__(self,real, img):
         self.real = real
         self.img = img
-
+# 
     
     def showNumber(self):
         print(self.real,"i +",self.img,"j")
