b1143e9611a4a55daebab8d0590e0c8ae56a10ba
Misbah yousaf <107263622+Misbahkk@users.noreply.github.com>
2024-07-29 22:07:33 +0500

    Update 07_sortRows.py

--- day-02/07_sortRows.py
+++ day-02/07_sortRows.py
@@ -4,7 +4,7 @@ import pandas as pd
 """Pandas DataFrame is a two-dimensional size-mutable, potentially heterogeneous tabular data structure with labeled axes (rows and columns)."""
 '''We often need to do certain operations on both rows and columns while handling the data.'''
 #Let’s see how to sort rows in Pandas DataFrame.
-
+# 
 data =  {'name': ['Simon', 'Marsh', 'Gaurav', 'Alex', 'Selena'],  
         'Maths': [8, 5, 6, 9, 7],  
         'Science': [7, 9, None, 4, 7], 
